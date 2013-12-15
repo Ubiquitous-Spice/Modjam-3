0e38ed99d410d8b6ba8dded6eb013c685b5315e3
Guilherme Espada <porcariadagata@gmail.com>
2013-12-15 11:44:50 +0000

    Render the things. :D
    H Y P E

--- src/main/java/com/github/ubiquitousspice/mobjam/proxy/ClientProxy.java
+++ src/main/java/com/github/ubiquitousspice/mobjam/proxy/ClientProxy.java
@@ -1,11 +1,15 @@
 package com.github.ubiquitousspice.mobjam.proxy;
 
 import com.github.ubiquitousspice.mobjam.blocks.ZombieBeacon;
+import com.github.ubiquitousspice.mobjam.entities.EntitySwarmZombie;
+import com.github.ubiquitousspice.mobjam.entities.SwarmZombieRenderer;
 import com.github.ubiquitousspice.mobjam.gamemodehack.GuiReplaceHandler;
 import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.entity.RenderZombie;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy
@@ -14,6 +18,7 @@ public class ClientProxy extends CommonProxy
 	public void registerRenderer()
 	{
 		ClientRegistry.bindTileEntitySpecialRenderer(ZombieBeacon.ZombieBeaconTE.class, new ZombieBeacon.ZombieBeaconTESR());
+		RenderingRegistry.registerEntityRenderingHandler(EntitySwarmZombie.class, new SwarmZombieRenderer());
 	}
 
 	@Override
